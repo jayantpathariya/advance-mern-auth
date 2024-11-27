5fa11e2be01e2269abc3a9b50dc42d9f8a183836
Jayant Pathariya <jayantmehra02@gmail.com>
2024-11-27 11:29:31 +0530

    feat: implement email sending functionality; add resend client integration, email templates, and configuration updates

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,17 +1,19 @@
-import { getEnv } from "@common/utils/get-env";
+import { getEnv } from '@common/utils/get-env';
 
 const appConfig = () => ({
-  NODE_ENV: getEnv("NODE_ENV", "development"),
-  APP_ORIGIN: getEnv("APP_ORIGIN", "localhost"),
-  PORT: getEnv("PORT", "5000"),
-  BASE_PATH: getEnv("BASE_PATH", "/api/v1"),
-  MONGO_URI: getEnv("MONGO_URI"),
+  NODE_ENV: getEnv('NODE_ENV', 'development'),
+  APP_ORIGIN: getEnv('APP_ORIGIN', 'localhost'),
+  PORT: getEnv('PORT', '5000'),
+  BASE_PATH: getEnv('BASE_PATH', '/api/v1'),
+  MONGO_URI: getEnv('MONGO_URI'),
   JWT: {
-    SECRET: getEnv("JWT_SECRET"),
-    EXPIRES_IN: getEnv("JWT_EXPIRES_IN"),
-    REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET"),
-    REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN"),
+    SECRET: getEnv('JWT_SECRET'),
+    EXPIRES_IN: getEnv('JWT_EXPIRES_IN'),
+    REFRESH_SECRET: getEnv('JWT_REFRESH_SECRET'),
+    REFRESH_EXPIRES_IN: getEnv('JWT_REFRESH_EXPIRES_IN'),
   },
+  RESEND_API_KEY: getEnv('RESEND_API_KEY'),
+  MAILER_SENDER: getEnv('MAILER_SENDER'),
 });
 
 export const config = appConfig();
---
+++ backend/src/mailers/mailer.ts
@@ -0,0 +1,32 @@
+import { resend } from '@mailers/resend-client';
+
+import { config } from '@/config/app.config';
+
+type Params = {
+  to: string | string[];
+  subject: string;
+  text: string;
+  html: string;
+  from?: string;
+};
+
+const mailer_sender =
+  config.NODE_ENV === 'development'
+    ? `no-reply <onboarding@resend.dev>`
+    : `no-reply <${config.MAILER_SENDER}>`;
+
+export const sendEmail = async ({
+  to,
+  subject,
+  text,
+  html,
+  from = mailer_sender,
+}: Params) => {
+  await resend.emails.send({
+    from,
+    to: Array.isArray(to) ? to : [to],
+    subject,
+    text,
+    html,
+  });
+};
